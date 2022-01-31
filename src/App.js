49ca43530d2117e5bdf8bfd3c79703a859164023
Le Quang Dong <dong.lq@hodfords>
2022-01-31 16:58:46 +0700

    run

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >123
+        >
           Learn React PR
         </a>
       </header>
